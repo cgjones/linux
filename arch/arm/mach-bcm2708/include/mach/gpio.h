wziard/adafruit-raspberrypi-linux

bcm2708: Add spare IRQs, GPIOs for MFDs (e.g. STMPE610)

arch/arm/mach-bcm2708/include/mach/gpio.h
@@ -9,7 +9,8 @@
 #ifndef __ASM_ARCH_GPIO_H
 #define __ASM_ARCH_GPIO_H
 
-#define ARCH_NR_GPIOS 54 // number of gpio lines
+//#define ARCH_NR_GPIOS 54 // number of gpio lines
+#define ARCH_NR_GPIOS 154 // number of gpio lines (plus 100 extra)
 
 #define gpio_to_irq(x)	((x) + GPIO_IRQ_START)
 #define irq_to_gpio(x)	((x) - GPIO_IRQ_START)

cgjones/linux

Conflicts:
	arch/arm/mach-bcm2708/include/mach/gpio.h
	arch/arm/mach-bcm2708/include/mach/irqs.h

arch/arm/mach-bcm2708/include/mach/gpio.h
@@ -9,7 +9,8 @@
 #ifndef __ASM_ARCH_GPIO_H
 #define __ASM_ARCH_GPIO_H
 
-#define BCM2708_NR_GPIOS 54 // number of gpio lines
+//#define BCM2708_NR_GPIOS 54 // number of gpio lines
+#define BCM2708_NR_GPIOS 154 // number of gpio lines (plus 100 extra)
 
 #define gpio_to_irq(x)	((x) + GPIO_IRQ_START)
 #define irq_to_gpio(x)	((x) - GPIO_IRQ_START)